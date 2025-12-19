2ef9f0fb1dce86040ca31a007f12a152eb0c4372
SaiyyedKaif <146384933+SaiyyedKaif@users.noreply.github.com>
2025-12-20 01:51:45 +0530

    Change server host to '0.0.0.0' in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
     return {
       server: {
         port: 3000,
-        host: 'autoviz-insight.onrender.com',
+        host: '0.0.0.0',
         
       },
       plugins: [react()],
